0aa17412f23dacb12deacbdc0a2194c459b1b700
Evan Mitchell <etmitchell2022@gmail.com>
2026-05-28 23:04:25 -0400

    Add CSRF protocol for frame building

---
+++ src/crsf-service/crsf_protocol.py
@@ -0,0 +1,11 @@
+"""
+CRSF Protocol Frame Builder
+"""
+
+
+class CRSFProtocol:
+    def __init__(self):
+        pass
+
+    def build_rc_channels_frame(self, channels: list[int]) -> bytes:
+        pass
